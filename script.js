f3df51d6566ccb8d14ae88c7e578c1086dc48d9c
ejazahmad18 <numen.ejaz@gmail.com>
2025-12-19 20:17:48 +0530

    Solved-Question-4

--- script.js
+++ script.js
@@ -42,11 +42,28 @@
 // }
 // console.log(product.function())
 
-let product={
-    cloths:"jeans",
-    price:3500,
-  discountprice:  function(){
-      return  this.price -200
+// let product={
+//     cloths:"jeans",
+//     price:3500,
+//   discountprice:  function(){
+//       return  this.price -200
+//     }
+// }
+// console.log(product.discountprice())
+
+//Question 4: create a car class with the following
+//brnad
+//speed
+//a drive two method that prints the car brnad and speed
+
+class car{
+    constructor(brand,speed){
+        this.brand=brand;
+        this.speed=speed;
+    }
+    drive(){
+        return this.brand +"--"+ this.speed;
     }
 }
-console.log(product.discountprice())
+let car1=new car("maruti",160)
+console.log(car1.drive())
