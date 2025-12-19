61cca111316197c9174251e4445b710d6c19bc97
ejazahmad18 <numen.ejaz@gmail.com>
2025-12-19 18:00:50 +0530

    Solved-First-Question

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Section 1: Objects and oops thinking ( Foundation )
+
+//Question 1: Create a user object that stores name and email and has a login method which prints "user logged in"
+let user={
+ 
+    name:"ejaz",
+    email:"ejazahmadbh18@gmail.com",
+    login:function(){
+        console.log("logged in")
+    }
+
+}
+user.login()
